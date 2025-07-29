05ff2fc9dea0016f1684f8e0e0dda99d3dc56865
revallalokesh <lokeshrevalla3@gmail.com>
2025-07-29 17:57:50 +0530

    backend url

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,3 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/notes', notesRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
