cda06e20c36274f09d07255d562ed0103c41c8d7
revallalokesh <lokeshrevalla3@gmail.com>
2025-07-29 18:29:57 +0530

    g

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,7 +3,5 @@
 import app from '../app.js';
 import { createServer } from 'http';
 
-export default function handler(req, res) {
-  const server = createServer((req, res) => app(req, res));
-  return server.emit('request', req, res);
-}
+export default serverless(app);
+
