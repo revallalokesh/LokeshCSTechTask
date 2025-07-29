23347512db2a5095fa1f483cd7d50c4b513b026c
revallalokesh <lokeshrevalla3@gmail.com>
2025-07-29 17:43:20 +0530

    url updated in backend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://internship-task-e401cwt45-lokeshrevalla3-gmailcoms-projects.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
